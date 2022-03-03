cda938b3b2fbf9069360404680ae88ccc311a1ec
unknown <delovyepisma@gmail.com>
2022-03-03 19:09:39 +0300

    Changed image output and corrected variable names

--- DB.js
+++ DB.js
@@ -1,12 +1,12 @@
 const db = {
-  people: [
+  postsMessage: [
     {
       photo: 'https://avatars.mds.yandex.net/i?id=2cac8eea95e2d083f3bb241e38c36076-5874989-images-thumbs&n=13&exp=1',
-      post: 'text',
+      post: 'Вчера видели енотов. Было очень круто!',
     },
     {
-      photo: 'https://avatars.mds.yandex.net/i?id=bb94c8faf7e7a9c4a7588a05a230e5a8-5490022-images-thumbs&n=13&exp=1',
-      post: 'texttext',
+      photo: 'https://avatars.mds.yandex.net/i?id=bc83a617d46968a78185ea76cce8b7b4-5870008-images-thumbs&n=13&exp=1',
+      post: 'Прошедшая выставка роботов нас очень удивила. А вы были на подобных мероприятиях?',
     },
   ],
 }
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const req = require('express/lib/request')
 const path = require('path')
 const { db } = require('./DB')
 
@@ -10,19 +11,20 @@ server.set('views', path.join(__dirname, 'src', 'views'))
 server.use(express.urlencoded({extended: true}))
 
 server.get('/', (req, res) => {
-  const usersQuery = req.query
-  let peopleForRender = db.people
+  const postsQuery = req.query
 
-  if (usersQuery.limit !== undefined && Number.isNaN(+usersQuery.limit) === false) {
-    peopleForRender = db.people.slice(0, usersQuery.limit)
+  let postsForRender = db.postsMessage
+ 
+  if (postsQuery.limit !== undefined && Number.isNaN(+postsQuery.limit) === false) {
+    postsForRender = db.postsMessage.slice(0, postsQuery.limit)
   }
 
-  res.render('main', {listOfPeople: peopleForRender})
+  res.render('main', {listOfPosts: postsForRender})
 })
 
 server.post('/photobook', (req, res) => {
-  const dataFromForm = req.body
-  db.people.push(dataFromForm)
+  const newPost = req.body
+  db.postsMessage.push(newPost)
 
   res.redirect('/')
 })
