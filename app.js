1ceba93d0920b47c8a1579429c0e9c16b19e0a92
unknown <delovyepisma@gmail.com>
2022-03-04 20:24:59 +0300

    added query parameter

--- app.js
+++ app.js
@@ -17,10 +17,30 @@ server.get('/', (req, res) => {
  
   if (postsQuery.limit !== undefined && Number.isNaN(+postsQuery.limit) === false) {
     postsForRender = db.postsMessage.slice(0, postsQuery.limit)
+  } else if (postsQuery.reverse !== undefined) { //вариант 3. Работает криво
+    postsForRender = db.postsMessage.reverse()
   }
 
   res.render('main', {listOfPosts: postsForRender})
+
+//вариант 1
+  // let reversePhoto = db.postsMessage.reverse()
+  // if (postsQuery.reverse === true) {
+  //   res.render('main', {listOfPeople: reversePhoto})
+  // } 
 })
+
+
+//вариант 2
+// server.get('/', (req, res) => {
+//   const postsRevQuery = req.query.reverse
+//   let reversePhoto = db.postsMessage
+//   if (postsRevQuery.reverse === true) {
+//     reversePhoto = db.postsMessage.reverse()
+//   } 
+//   res.render('main', {listOfPeople: reversePhoto})
+// })
+
 
 server.post('/photobook', (req, res) => {
   const newPost = req.body
